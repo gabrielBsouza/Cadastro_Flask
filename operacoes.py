310196e59b70e9ac055a6eaabc4ed9b9507cba5a
GABRIEL BIFON DE SOUZA <gabriel.bsouza13@senacsp.edu.br>
2022-06-21 22:15:52 -0300

    first commit

---
+++ operacoes.py
@@ -0,0 +1,56 @@
+import conexao
+import this
+this.msg = ""
+db_connection = conexao.conexao()
+con = db_connection.cursor()
+
+def inserir(nome, telefone, endereco, dataDeNascimento):
+    try:
+        sql = "Insert into pessoa(codigo, nome, telefone, endereco, dataDeNascimento) values('', '{}', '{}', '{}', '{}')".format(nome, telefone, endereco, dataDeNascimento)
+        con.execute(sql)
+        db_connection.commit()
+        return con.rowcount,"Inserido!"
+    except Exception as erro:
+        return erro
+
+def consultar():
+    try:
+        sql = "select * from pessoa"
+        con.execute(sql)
+
+        this.msg = ""
+        for (codigo, nome, telefone, endereco, dataDeNascimento) in con:
+            this.msg = this.msg + "Código: {} ||| Nome: {} ||| Telefone: {} ||| Endereço: {} ||| Data de nascimento: {}".format(codigo, nome, telefone, endereco, dataDeNascimento)
+        return this.msg
+    except Exception as erro:
+        return erro
+
+def consultarPorCodigo(cod):
+    try:
+        sql = "select * from pessoa where codigo = '{}'".format(cod)
+        con.execute(sql)
+
+        this.msg = ""
+        this.msg = "Nenhum dado Encontrado!"
+        for(codigo, nome, telefone, endereco, dataDeNascimento) in con:
+            if int(codigo) == int(cod):
+                this.msg = "Código: {} ||| Nome: {} ||| Telefone: {} ||| Endereço: {} ||| Data de Nascimento: {}".format(codigo, nome, telefone, endereco, dataDeNascimento)
+                return this.msg
+        return this.msg
+    except Exception as erro:
+        return erro
+
+def consultarPorCodigo(cod):
+    try:
+        sql = "select * from pessoa where codigo = '{}'".format(cod)
+        con.execute(sql)
+
+        this.msg = ""
+        this.msg = "Nenhum dado encontrado!"
+        for(codigo, nome, telefone, endereco, dataDeNascimento) in con:
+            if int(codigo) == int(cod):
+                this.msg = "Código: {} ||| Nome: {} ||| Telefone: {} ||| Endereço: {} ||| Data de nascimento: {}".format(codigo, nome, telefone, endereco, dataDeNascimento)
+                return this.msg
+        return this.msg
+    except Exception as erro:
+        return erro
